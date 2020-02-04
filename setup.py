65ea301e2c77c846c215c2218ae709dcead53f74
Miha Moskon <miha.moskon@fri.uni-lj.si>
2020-02-04 16:52:52 +0100

    setup

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',     
-    'Intended Audience :: Researchers',     
+    'Intended Audience :: Developers',     
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   
     'Programming Language :: Python :: 3',      
